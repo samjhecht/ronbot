6510fb48362a3537a9ff555bdcbd5839a4206c4c
samjhecht <samjhecht@gmail.com>
2015-07-13 19:54:05 -0700

    adding ronisms

--- ronbot.js
+++ ronbot.js
@@ -1,16 +1,22 @@
 module.exports = function (req, res, next) {
   
-  var ronisms = [
-    'For your convenience, your request has been denied.',
-    'Don\'t spill your candy in the lobby.',
-    'Quit chasing tennis balls.',
-    'For your convenience, the ice machine is located in the basement.',
-    'Just f***ing sell!',
-    'Do you think this kind of desperation is attractive?',
-    'Stop selling after the close!'
-  ]
-
   var userName = req.body.user_name;
+
+  var ronisms = [
+      'For your convenience, your request has been denied.',
+      'Don\'t spill your candy in the lobby.',
+      'Quit chasing tennis balls.',
+      'For your convenience, the ice machine is located in the basement.',
+      'Just f***ing sell!',
+      'Do you think this kind of desperation is attractive?',
+      'Stop selling after the close!',
+      'Have you updated your forecast in Salesforce?',
+      'I\'ll buy take Jaeger shots if you close a deal!',
+      'Oh you big wuss.',
+      'What\'s the probability that they\'ll close this month?'
+    ]
+
+
   var botPayload = {
     text : ronisms[Math.floor(Math.random() * ronisms.length)]
   };
